0ec7ef62813c505d6de803f8ebd7b92e494d74f8
Mehran Zeidi <zeidimehran@gmail.com>
2024-06-29 10:30:19 +0200

    Fix the generator.

--- generator_model.py
+++ generator_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from adain_model import AdaIN
 
+
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, down=True, use_act=True, use_in=False, **kwargs):
         super().__init__()
@@ -22,25 +23,21 @@ class ConvBlock(nn.Module):
 class ResidualBlock(nn.Module):
     def __init__(self, channels, adain):
         super().__init__()
-        self.adain1 = adain
-        self.adain2 = adain
-        self.block = nn.Sequential(
-            ConvBlock(channels, channels, kernel_size=3, padding=1, use_in=False),
-            ConvBlock(channels, channels, use_act=False, kernel_size=3, padding=1, use_in=False),
-        )
+        self.adain = adain
+        self.conv1 = ConvBlock(channels, channels, kernel_size=3, padding=1, use_in=False)
+        self.conv2 = ConvBlock(channels, channels, use_act=False, kernel_size=3, padding=1, use_in=False)
 
-    def forward(self, x, style):
-        out = self.block[0](x)
-        out = self.adain(out, style)
-        out = self.block[1](out)
-        out = self.adain(out, style)
+    def forward(self, x, content_features, style_features):
+        out = self.conv1(x)
+        out = self.adain(out, content_features, style_features)
+        out = self.conv2(out)
+        out = self.adain(out, content_features, style_features)
         return x + out
 
 class Generator(nn.Module):
-    def __init__(self, img_channels, num_features=64, num_residuals=9, adain):
+    def __init__(self, adain, img_channels, num_features=64, num_residuals=9):
         super().__init__()
-        self.adain1 = adain
-        self.adain2 = adain
+        self.adain = adain
         self.initial = nn.Sequential(
             nn.Conv2d(
                 img_channels,
@@ -50,6 +47,7 @@ class Generator(nn.Module):
                 padding=3,
                 padding_mode="reflect",
             ),
+            nn.InstanceNorm2d(num_features),
             nn.ReLU(inplace=True),
         )
         self.down_blocks = nn.ModuleList(
@@ -63,8 +61,8 @@ class Generator(nn.Module):
         )
         self.up_blocks = nn.ModuleList(
             [
-                ConvBlock(num_features * 4, num_features * 2, down=False, kernel_size=3, stride=2, padding=1, output_padding=1, use_in=False),
-                ConvBlock(num_features * 2, num_features, down=False, kernel_size=3, stride=2, padding=1, output_padding=1, use_in=False),
+                ConvBlock(num_features * 4, num_features * 2, down=False, kernel_size=3, stride=2, padding=1, output_padding=1, use_in=True),
+                ConvBlock(num_features * 2, num_features, down=False, kernel_size=3, stride=2, padding=1, output_padding=1, use_in=True),
             ]
         )
         self.last = nn.Conv2d(
@@ -76,12 +74,12 @@ class Generator(nn.Module):
             padding_mode="reflect",
         )
 
-    def forward(self, x, style):
+    def forward(self, x, content_features, style_features):
         x = self.initial(x)
         for layer in self.down_blocks:
             x = layer(x)
         for res_block in self.res_blocks:
-            x = res_block(x, style)
+            x = res_block(x, content_features, style_features)
         for layer in self.up_blocks:
             x = layer(x)
         return torch.tanh(self.last(x))
@@ -92,13 +90,13 @@ def load_adain_weights(adain, path):
 def test():
     img_channels = 3
     img_size = 256
-    style_dim = 512
     x = torch.randn((2, img_channels, img_size, img_size))
-    style = torch.randn((2, style_dim))
-    adain = AdaIN(style_dim, 512)
+    content_images = torch.randn((2, img_channels, img_size, img_size))
+    style_images = torch.randn((2, img_channels, img_size, img_size))
+    adain = AdaIN()
     load_adain_weights(adain, '/home/mehran/Git/SRStyle/model_state.pth')
-    gen = Generator(img_channels, 9, adain=adain)
-    print(gen(x, style).shape)
+    gen = Generator(img_channels, 9)
+    print(gen(x, content_images, style_images).shape)
 
 if __name__ == "__main__":
     test()
